b04d594e102a4109219562190ff49ddfac970c33
Vitali Vinahradski <connormclaud@gmail.com>
2012-04-25 14:34:46 +0300

    Solution in python for problem 1 added

---
+++ python/Problem 001/solution.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+# Task description:
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+sum = reduce(lambda x, y: x + y
+             if y % 3 == 0 or y % 5 == 0 else x, xrange(1000), 0)
+
+print 'Sum of all the multiples of 3 or 5 below 1000 is %s' % sum
+
