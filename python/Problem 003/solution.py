69a761751193fdca761a6976dd5d31ee42d17703
Vitali Vinahradski <connormclaud@gmail.com>
2014-12-22 23:45:32 +0300

    Added solution for the Project euler's problem 3 in python. Calculating largest prime factor for a big integer.

--- python/Problem 003/solution.py
+++ python/Problem 003/solution.py
@@ -21,5 +21,66 @@
 
 #What is the largest prime factor of the number 600851475143 ?
 
-print 'The largest prime factor of' +\
-    ' the number 600851475143 is %s' % 'TO BE CALCULATED'
+# we will use simple factorizing algorithm from Knuth's The art of computer
+# programming Vol2 4.5.4 Factoring into Primes Algorithm A (Factoring by division)
+# This algorithm is simple but not efficient for large number.
+
+# we need prime numbers to divide for. Fortunately, this algorithm will work
+# even if a list of numbers contains some numbers which are not prime
+# So we can add 2 and 4 for items after first three. This list will contain
+# all prime numbers and more.
+from math import sqrt
+def prime_like_number_generator(stop_value=500000):
+    for i in [2, 3, 5]:
+        item = i
+        yield item
+    while(item < stop_value):
+        item += 2
+        yield item
+        item += 4
+        yield item
+
+number = 600851475143
+#largest prime factor can't be greater than square of N
+primes = list(prime_like_number_generator(sqrt(number)))
+
+def prime_factors(number):
+    # step A1
+    k = 0
+    n = number
+    quotient = number
+    while(quotient > primes[k]):
+        # step A2
+        if(n == 1):
+            return
+        # step A3
+        quotient = n / primes[k]
+        remainder = n % primes[k]
+        # step A4
+        if(remainder == 0):
+            # step A5
+            prime_factor = primes[k]
+            yield prime_factor
+            n = quotient
+            # return to step A2
+        else:
+            # step A6
+            k += 1
+            # return to step A3
+    # step A7
+    prime_factor = n
+    yield prime_factor
+
+
+
+factors = list(prime_factors(number))
+
+# check whether factors are right. Just multiply all of them
+multiply = reduce(lambda x, y: x * y, factors, 1)
+if multiply != number:
+    raise "Logic is broken"
+
+largest_prime_factor = factors[-1]
+
+print 'The largest prime factor of' + \
+    ' the number %s is %s' % (number, largest_prime_factor)
