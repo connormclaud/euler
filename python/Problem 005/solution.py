8dcb63efca1114f5eeef6c0c81aad0952ebeec5e
Vitali Vinahradski <connormclaud@gmail.com>
2014-12-26 14:57:07 +0300

    Problem 5. Calculate least common multiple for set of numbers

---
+++ python/Problem 005/solution.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+# Task description:
+# Compute least common multiplier for all integers from 1 to 160
+
+limit = 160
+#we can decrease number of calculations by dropping first half of input.
+input = range(limit/2 + 1, limit + 1, 1)
+
+# greatest common divisor by Euclid's algorithm
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+# least common multiple for two numbers
+def lcm(a, b):
+    return a / gcd(a, b) * b
+
+result = 1
+for i in input:
+    result = lcm(result, i)
+
+print "the smallest positive number that is evenly divisible"+\
+    " by all of the numbers from 1 to %s is %s" % (limit, result)
