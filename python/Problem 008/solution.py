f539e88ad65362b6e81dfb75f3c5121a7fb218fe
Vitali Vinahradski <connormclaud@gmail.com>
2015-01-05 20:06:14 +0300

    Find subarray with a maximum product solution.

---
+++ python/Problem 008/solution.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+# Task description:
+# Find subarray of 13 elements which product is a maximum in 100000 element array
+
+import random
+
+# generate random array
+digits = 100000
+array = [int(10 * random.random()) for i in xrange(digits)]
+
+elements = 42
+max_value = 0
+max_start = 0
+
+# straightforward algorithm.
+for index, item in enumerate(array):
+    if index + elements > len(array):
+        break
+    # calculate a product of next elements
+    product = reduce(lambda x, y: array[y + index] * x, xrange(elements), 1)
+    if product > max_value:
+        # if it is a maximum, remember it
+        max_value = product
+        max_start = index
+# print the product
+print ('The %s adjacent items in the %s-member array'
+    ' that have the greatest product are: ') % (elements, digits)
+print ' x '.join(map(lambda x: str(array[x + max_start]), xrange(elements))),
+print '=',
+print max_value
