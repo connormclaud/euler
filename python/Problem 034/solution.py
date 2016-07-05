b0fa3a92057615401bb33ecd1bcdad44c0eaab86
Vitali Vinahradski <connormclaud@gmail.com>
2016-07-05 16:20:40 +0300

    Find the sum of all factorions
    solved by hardcoding

---
+++ python/Problem 034/solution.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+# Task description:
+# Find the sum of all numbers which are equal to the sum of the
+# factorial of their digits.
+# Solution:
+# There are just four factorions (in base 10) and
+# they are 1, 2, 145 and 40585 (sequence A014080 in OEIS)
+
+print('The sum of all numbers which are equal to the sum '
+      'of the factorial of their digits is', 1 + 2 + 145 + 40585)
+#
