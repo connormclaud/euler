9bece0c95cbd0b67b70610d6455ba6fa07a8346a
Vitali Vinahradski <connormclaud@gmail.com>
2016-07-22 15:46:41 +0300

    Totient maximum

---
+++ python/Problem 069/solution.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+# Task description:
+# Totient maximum
+
+# Solution:
+# We need to maximize number of prime factors in order to find totient maximum
+
+def sieve(limit=10000):
+    ''' Eratosphenes sieve '''
+    sieve = [True, True, False] + [
+        False if x % 2 else True for x in range(3, limit)]
+    for candidate in range(3, int(limit ** 0.5) + 1, 2):
+        if not sieve[candidate]:
+            # found prime number
+            # mark all composites where found prime is a factor
+            i = 2
+            while (i*candidate < limit):
+                sieve[i*candidate] = True
+                i += 1
+
+    return sieve
+
+
+def primes_gen(sieve):
+    yield from (number for number,
+                is_composite in enumerate(sieve) if not is_composite)
+
+
+def find(limit=100000):
+    result = 1
+    for prime in primes_gen(sieve(limit)):
+        if result * prime < limit:
+            result *= prime
+        else:
+            break
+    return result
+
+
+print('Totient maximum less than 100000 is', find())
